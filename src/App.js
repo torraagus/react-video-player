086f55a450743cba0be68b727421f49857b7696e
broana-ait <torra.agustinignacio@gmail.com>
2020-06-10 18:34:41 -0300

    update App component to show video player

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactPlayer from "react-player";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<ReactPlayer
+				controls
+				width="900px"
+				height="500px"
+				url="https://www.youtube.com/watch?v=n2-w5v5LI2s"
+			/>
+		</div>
+	);
 }
 
 export default App;
